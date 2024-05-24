b79e2bc805c7686bb1df188d3d9f51e6666ef5f2
Guilherme Prado <guilhermeprado-09@hotmail.com>
2024-05-23 21:48:48 -0300

    username initial value

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -3,8 +3,9 @@ import { Container, Form, FormError, Header } from './styles'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-import handler from '../api/hello'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const registerFormSchema = z.object({
   username: z
@@ -26,10 +27,19 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
   })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
     console.log(data)
