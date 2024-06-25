9b1472890165a3e8f2542b2903415a9f479f2e6e
Guilherme Prado <guilhermeprado-09@hotmail.com>
2024-06-25 20:36:02 -0300

    change sqlite to mysql

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/lib/prisma'
-import dayjs from 'dayjs'
+// import dayjs from 'dayjs'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handle(
@@ -42,5 +42,14 @@ export default async function handle(
     )
   })
 
-  return res.json({ blockedWeekDays })
+  const blockedDatesRaw = await prisma.$queryRaw`
+    SELECT * 
+    FROM schedulings
+
+    WHERE S.user_id = ${user.id}
+      AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+
+  `
+
+  return res.json({ blockedWeekDays, blockedDatesRaw })
 }
